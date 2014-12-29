a52c1ad14ac9c0483318e8b32067e7c556a22daa
Bladymir Tellez <btellez@gmail.com>
2014-12-28 23:14:33 -0800

    Implement Noun Project Api Methods and add UI Handler to post to UI Thread.

--- app/src/main/java/com/btellez/solidandroid/network/OkNounProjectApi.java
+++ app/src/main/java/com/btellez/solidandroid/network/OkNounProjectApi.java
@@ -1,12 +1,17 @@
 package com.btellez.solidandroid.network;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import com.btellez.solidandroid.configuration.Configuration;
+import com.btellez.solidandroid.model.Icon;
 import com.btellez.solidandroid.model.IconParser;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -17,20 +22,42 @@ import javax.inject.Inject;
 public class OkNounProjectApi implements  NounProjectApi {
     @Inject Configuration configuration;
     @Inject IconParser iconParser;
-    private OkHttpClient client = new OkHttpClient();
+
+    // Implementation Related Dependencies
+    private NounProjectOAuth oAuth;
+    private OkHttpClient client;
+
+    public OkNounProjectApi(Configuration configuration, IconParser iconParser) {
+        this.configuration = configuration;
+        this.iconParser = iconParser;
+
+        // Fulfil Implementation Related Dependencies
+        this.client = new OkHttpClient();
+        this.oAuth = new NounProjectOAuth(configuration.getNounProjectApiKeys());
+    }
 
     @Override
     public void search(String term, final Callback callback) {
         String endpoint = new EndpointBuilder().buildSearchUrl(configuration, term);
-        Request request = new Request.Builder().url(endpoint).build();
+        Request request = buildRequest(endpoint, NounProjectOAuth.RequestType.GET);
         client.newCall(request).enqueue(new OkHttpCallback(callback, "icons"));
     }
 
     @Override
     public void recent(final Callback callback) {
         String endpoint = new EndpointBuilder().buildRecentUrl(configuration);
-        Request request = new Request.Builder().url(endpoint).build();
+        Request request = buildRequest(endpoint, NounProjectOAuth.RequestType.GET);
         client.newCall(request).enqueue(new OkHttpCallback(callback, "recent_uploads"));
+    }
+
+    private Request buildRequest(String endpoint, NounProjectOAuth.RequestType method) {
+        String oAuthString = oAuth.withEnpoint(endpoint)
+                                    .withRequestType(method)
+                                    .getOAuthHeader();
+
+        return new Request.Builder().url(endpoint)
+                                    .addHeader("Authorization", oAuthString)
+                                    .build();
     }
 
     /**
@@ -42,18 +69,33 @@ public class OkNounProjectApi implements  NounProjectApi {
     private class OkHttpCallback implements  com.squareup.okhttp.Callback {
         public Callback callback;
         public String dataKey;
+        private Handler uiHandler = new Handler(Looper.getMainLooper());
 
         private OkHttpCallback(Callback callback, String dataKey) {
             this.callback = callback;
             this.dataKey = dataKey;
         }
 
-        @Override public void onFailure(Request request, IOException e) {
-            callback.onFailure(e);
+        @Override public void onFailure(final Request request,final IOException e) {
+            // We need to post update to the UI thread.
+            uiHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    callback.onFailure(e);
+                }
+            });
         }
 
-        @Override public void onResponse(Response response) throws IOException {
-            callback.onSuccess(iconParser.fromJson(response.body().string(), dataKey));
+        @Override public void onResponse(final Response response) throws IOException {
+            final List<Icon> result = iconParser.fromJson(response.body().string(), dataKey);
+            // We need to post update to the UI thread.
+            uiHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                        callback.onSuccess(result);
+                }
+            });
+
         }
     }
 }
