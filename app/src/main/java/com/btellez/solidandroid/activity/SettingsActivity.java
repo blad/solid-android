3b7348cfc28a3977ac65b3e987ff4674f0c05ce4
Bladymir Tellez <btellez@gmail.com>
2015-01-14 21:55:48 -0800

    SettingActivity add placeholder for toggling history settings.

--- app/src/main/java/com/btellez/solidandroid/activity/SettingsActivity.java
+++ app/src/main/java/com/btellez/solidandroid/activity/SettingsActivity.java
@@ -2,9 +2,11 @@ package com.btellez.solidandroid.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import com.btellez.solidandroid.R;
+import com.btellez.solidandroid.configuration.KeyValueStore;
 
 
 public class SettingsActivity extends Activity {
@@ -22,6 +24,15 @@ public class SettingsActivity extends Activity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
+
+            findPreference(getString(R.string.pref_key_enable_history))
+                    .setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(Preference preference) {
+                            // TODO: Clear History.
+                            return true;
+                        }
+                    });
         }
     }
 }
